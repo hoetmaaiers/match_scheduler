cc832fc27d6704af87eedb865625a22d4a3dfeb7
Hoetmaaiers <houdmeyers@gmail.com>
2014-05-07 23:04:28 +0200

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BadmintonSheduler::Application.config.secret_key_base = 'f994612727504bfa3de18b7e26436ce060485ea8491abd0877538732bc17b8569c099546af5d01bbf51e7e45f931362f81d6fc2779111ae1a09aa94d62a4f8b4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BadmintonSheduler::Application.config.session_store :cookie_store, key: '_badminton-sheduler_session'
